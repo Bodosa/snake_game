a8400a32377d02afa75d68f07d5a6125dd579a60
Bodosa <sbodova24@students.oamk.fi>
2025-11-14 21:53:07 +0100

    Add files via upload

---
+++ snake.py
@@ -0,0 +1,49 @@
+from turtle import Turtle
+S_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    # TODO: 1. create snake body (3 squares)
+    def create_snake(self):
+        for position in S_POSITIONS:
+            new_s = Turtle("square")
+            new_s.color("white")
+            new_s.penup()
+            new_s.goto(position)
+            self.segments.append(new_s)
+
+    # TODO: 2. move snake and changing directions
+    def move(self):
+        # turn to the left the snake
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
---
+++ snake_game.py
@@ -0,0 +1,31 @@
+from turtle import Screen, Turtle
+from snake import Snake
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My snake game")
+
+# turn off the turtle animation
+screen.tracer(0)
+
+snake = Snake()
+
+# TODO: 3. control the snake (up, left, down, right) arrow keys
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+game_on = True
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+
+screen.exitonclick()
